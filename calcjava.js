74bc9771b75abcfe2912859b47e7ac0ab61714b1
Shazuli <Shazuli@users.noreply.github.com>
2018-01-19 22:35:22 +0100

    0.09
    La till `PI` och `^`. Den fungerar nästan.

--- calcjava.js
+++ calcjava.js
@@ -1,6 +1,8 @@
+var result = "";
+var degrees = true;
 window.onload = function() {
 
-	/*Number buttons.*/
+	/*Nummer knappar.*/
 	document.getElementById("b0").addEventListener("click", function() {
 		if (document.getElementById("txtArea").innerHTML == "0")
 			document.getElementById("txtArea").innerHTML = "";
@@ -52,10 +54,13 @@ window.onload = function() {
 			document.getElementById("txtArea").innerHTML = "";
 		document.getElementById("txtArea").innerHTML += "9";
 	});
+	/*Nummer knappar.*/
+	/*Special tecken.*/
 	document.getElementById("bp").addEventListener("click", function() {
 		/*if (document.getElementById("txtArea").innerHTML == "0")
 			document.getElementById("txtArea").innerHTML = "";*/
-		document.getElementById("txtArea").innerHTML += ".";
+		if (!document.getElementById("txtArea").innerHTML.includes("."))
+			document.getElementById("txtArea").innerHTML += ".";
 	});
 	document.getElementById("bE").addEventListener("click", function() {
 		if (document.getElementById("txtArea").innerHTML == "0")
@@ -65,23 +70,22 @@ window.onload = function() {
 	document.getElementById("bSin").addEventListener("click", function() {
 		if (document.getElementById("txtArea").innerHTML == "0")
 			document.getElementById("txtArea").innerHTML = "";
-		document.getElementById("txtArea").innerHTML += "Math.sin(";
+		document.getElementById("txtArea").innerHTML += "Sin(";
 	});
 	document.getElementById("bCos").addEventListener("click", function() {
 		if (document.getElementById("txtArea").innerHTML == "0")
 			document.getElementById("txtArea").innerHTML = "";
-		document.getElementById("txtArea").innerHTML += "Math.cos(";
+		document.getElementById("txtArea").innerHTML += "Cos(";
 	});
 	document.getElementById("bTan").addEventListener("click", function() {
 		if (document.getElementById("txtArea").innerHTML == "0")
 			document.getElementById("txtArea").innerHTML = "";
-		document.getElementById("txtArea").innerHTML += "Math.tan(";
+		document.getElementById("txtArea").innerHTML += "Tan(";
 	});
-	
 	document.getElementById("bSqr").addEventListener("click", function() {
 		if (document.getElementById("txtArea").innerHTML == "0")
 			document.getElementById("txtArea").innerHTML = "";
-		document.getElementById("txtArea").innerHTML += "Math.sqrt(";
+		document.getElementById("txtArea").innerHTML += "√(";
 	});
 	document.getElementById("bpv").addEventListener("click", function() {
 		if (document.getElementById("txtArea").innerHTML == "0")
@@ -93,35 +97,61 @@ window.onload = function() {
 			document.getElementById("txtArea").innerHTML = "";
 		document.getElementById("txtArea").innerHTML += ")";
 	});
-	/*Other stuff.*/
+	document.getElementById("bPI").addEventListener("click", function () {
+		if (document.getElementById("txtArea").innerHTML == "0")
+			document.getElementById("txtArea").innerHTML = "";
+		document.getElementById("txtArea").innerHTML += "π";
+	});
+	document.getElementById("bPO").addEventListener("click", function () {
+		document.getElementById("txtArea").innerHTML += "^";
+	});
+	/*Special tecken.*/
+	/*Andra grejer.*/
 	document.getElementById("bResult").addEventListener("click", function() {
 		try {
-			document.getElementById("txtArea").innerHTML = eval(document.getElementById("txtArea").innerHTML.toString());
+			result = document.getElementById("txtArea").innerHTML
+
+			if (result.includes("^")) {
+				var powerTo = powerOf(result);
+				// window.alert("\"" + result + "\"");
+				// window.alert("\"" + powerTo[0] + "\"");
+				// window.alert("\"" + powerTo[1] + "\"");
+				result = result.replace(powerTo[0].toString() + "^" + powerTo[1].toString(), "Math.pow(powerTo[0],powerTo[1])");
+			}
+
+			result = result.replace("Sin", "Math.sin");
+			result = result.replace("Cos", "Math.cos");
+			result = result.replace("Tan", "Math.tan");
+			result = result.replace("√", "Math.sqrt");
+			result = result.replace("π", "Math.PI");
+
+			result = result.replace(")(", ")*(");
+
+
+
+
+
+			result = eval(result.toString());
+			document.getElementById("txtArea").innerHTML = result;
 			//window.alert(result);
 		}
 		catch (e) {
 			window.alert(e);
 			document.getElementById("txtArea").innerHTML = "0";
+			result = "0";
 		}
 	});
-
 	document.getElementById("bDel").addEventListener("click", function() {
-
 		if (document.getElementById("txtArea").innerHTML.length < 2 && parseInt(document.getElementById("txtArea").innerHTML) > 0)
 			document.getElementById("txtArea").innerHTML = "0";
-		//result = "";
-		if (document.getElementById("txtArea").innerHTML.length > 1) {
+		if (document.getElementById("txtArea").innerHTML.length > 1)
 			document.getElementById("txtArea").innerHTML = document.getElementById("txtArea").innerHTML.substring(0, document.getElementById("txtArea").innerHTML.length-1);
-
-		}
 	});
-
 	document.getElementById("bClr").addEventListener("click", function() {
-
 		document.getElementById("txtArea").innerHTML = "0";
 	});
-
-	/*Operations.*/
+	/*Andra grejer.*/
+	/*Operationer.*/
 	document.getElementById("bAdd").addEventListener("click", function() {
 		document.getElementById("txtArea").innerHTML += "+";
 	});
@@ -137,18 +167,19 @@ window.onload = function() {
 	document.getElementById("bDiv").addEventListener("click", function() {
 		document.getElementById("txtArea").innerHTML += "/";
 	});
+	/*Operationer.*/
 
 	try {
 
 		/*fs = require("fs");*/
-  
-  
+
+
 		/*var cmd = "window.alert('funkar det?')";
 		eval(cmd);*/
-		
+
 		var ul = document.getElementById("konstanter");
 		var antL = 40;
-  
+
 		for (var i = 0; i<antL; i++) {
 			var li = document.createElement("LI");
 		  	li.innerHTML = "Konstanter";
@@ -161,14 +192,52 @@ window.onload = function() {
 		  	li.innerHTML = "Formler";
 		  	ul.appendChild(li);
 		}
-		
+
 	  }
 
-	  
+
 
 
 	  catch(e){window.alert("Error {" + e + "}");}
 
 
+	  /*var poop = powerOf("123^56");
+	  window.alert(poop[0]);
+	  window.alert(poop[1]);*/
+	  //window.alert('hej'.includes('g'));
 
+}
+
+function powerOf(str) {
+
+	var pSymbol = str.indexOf("^"),
+	 	num1 = 0,
+		num2 = 0,
+		min = 0,
+		max = 0;
+	var	cSymbol = pSymbol;
+
+
+	for (var i = pSymbol; i >= 0; i--) {
+		if (parseInt(str.charAt(i)) === parseInt(str.charAt(i), 10) || str.charAt(i) == ".") {
+			cSymbol--;
+		}
+	}
+
+	num1 = str.substring(cSymbol,pSymbol);
+	min = cSymbol;
+	cSymbol = pSymbol;
+	pSymbol++;
+	cSymbol++;
+
+	for (var i = 0; i < pSymbol; i++)  {
+		if (parseInt(str.charAt(i)) === parseInt(str.charAt(i)) || str.charAt(i) == ".") {
+			cSymbol++;
+		}
+	}
+	num2 = str.substring(pSymbol,cSymbol);
+	max = cSymbol;
+	//window.alert(min);
+	//window.alert(max);
+	return [num1, num2];
 }
