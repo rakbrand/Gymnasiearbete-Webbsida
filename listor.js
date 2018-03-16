b1519c907c160b1d54c1e5a0dcde3df6e6949349
Shazuli <Shazuli@users.noreply.github.com>
2018-03-16 11:00:57 +0100

    0.1.5
    Fixade filter funktionen och förminskade "2nd" knappen.

--- listor.js
+++ listor.js
@@ -3,15 +3,24 @@ function lists() {
     try {
 		var ul = document.getElementById("konstanter");
 		for (var i in KONST) {
-			var li = document.createElement("LI");
-		  	li.innerHTML = "<a>" +  i + " = " + KONST[i] + "</a>";
+            var li = document.createElement("LI");
+            var t = KONST[i];
+            //t = t.replace('e','^');
+            //t = t + 'fdfdf';
+            //li.innerHTML = "<a>$$\b" +  i + " = " + t + "$$</a>";
+            li.setAttribute('dataKONST', i + ": " + KONST[i]);
+		  	li.innerHTML = "<a>$$\b" +  i + " = " + t + "$$</a>";
 		  	ul.appendChild(li);
 		}
+        /*.setAttribute('data', "icon: 'base2.gif', url: 'output.htm', target: 'AccessPage', output: '1'");*/
+
+
 
 		ul = document.getElementById("formler");
 		for (var i in FORM) {
 			var li = document.createElement("LI");
-		  	li.innerHTML = "<a>$$\b" + i + ": " + FORM[i] + "$$</a>";
+              li.innerHTML = "<a>$$\b" + i + ": " + FORM[i] + "$$</a>";
+              li.setAttribute('dataFORM', i + ": " + FORM[i]);
 		  	ul.appendChild(li);
 		}
 
@@ -21,12 +30,14 @@ function lists() {
 function searchingKonst() {
     var input, filter, ul, li, a, i;
     input = document.getElementById("searchFilterKonst");
-    filter = input.value; //filter = input.value.toUpperCase();
+    filter = input.value.toUpperCase(); //filter = input.value.toUpperCase();
     ul = document.getElementById("konstanter");
     li = ul.getElementsByTagName("li");
     for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        if (a.innerHTML.indexOf(filter) > -1) { //if (a.innerHTML.toUpperCase().indexOf(filter) > -1)
+        //a = li[i].getElementsByTagName("a")[0];
+        a = li[i].getAttribute("dataKONST");
+        //console.log(a.innerHTML.toUpperCase().indexOf(filter));
+        if (a.toUpperCase().indexOf(filter) > -1) { //if (a.innerHTML.toUpperCase().indexOf(filter) > -1)
             li[i].style.display = "";
         } else {
             li[i].style.display = "none";
@@ -42,8 +53,10 @@ function searchingForm() {
     ul = document.getElementById("formler");
     li = ul.getElementsByTagName("li");
     for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) { //if (a.innerHTML.toUpperCase().indexOf(filter) > -1)
+        //a = li[i].getElementsByTagName("a")[0];
+        a = li[i].getAttribute("dataFORM");
+        //console.log(a.innerHTML.toUpperCase().indexOf(filter),a.innerHTML.toUpperCase());
+        if (a.toUpperCase().indexOf(filter) > -1) { //if (a.innerHTML.toUpperCase().indexOf(filter) > -1)
             li[i].style.display = "";
         } else {
             li[i].style.display = "none";
